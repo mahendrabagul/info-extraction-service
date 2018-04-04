f30ed239b470e9f1d913abcaf57c5009463424be
Mahendra Bagul <bagulm123@gmail.com>
2018-04-04 23:29:41 +0530

    updated values

--- src/main/java/io/github/knowmyminister/infoextractionservice/common/KMM_CONSTANTS.java
+++ src/main/java/io/github/knowmyminister/infoextractionservice/common/KMM_CONSTANTS.java
@@ -2,6 +2,5 @@ package io.github.knowmyminister.infoextractionservice.common;
 
 public class KMM_CONSTANTS {
     public static final String GOOGLE_URL = "http://www.google.com/search?q=";
-    public static final String GOOGLE_IMAGE_URL = "https://www.google.co.in/search?q={QUERY_STRING_VALUE}+politics&tbs=ic:color,isz:m,itp:face&tbm=isch&source=lnt";
     public static final String FILENAME = "src/main/resources/ministers.txt";
 }
--- src/main/java/io/github/knowmyminister/infoextractionservice/util/InfoExtracter.java
+++ src/main/java/io/github/knowmyminister/infoextractionservice/util/InfoExtracter.java
@@ -4,12 +4,17 @@ import io.github.knowmyminister.infoextractionservice.common.KMM_CONSTANTS;
 import io.github.knowmyminister.infoextractionservice.domain.Minister;
 import io.github.knowmyminister.infoextractionservice.selectors.SelectorsContants;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import sun.misc.BASE64Decoder;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
@@ -37,6 +42,23 @@ public class InfoExtracter {
                 e.printStackTrace();
             }
         }
+        System.out.println(images);
+        images.forEach(image -> decodeToImage(image));
+    }
+
+    public static BufferedImage decodeToImage(String imageString) {
+        BufferedImage image = null;
+        byte[] imageByte;
+        try {
+            BASE64Decoder decoder = new BASE64Decoder();
+            imageByte = decoder.decodeBuffer(imageString);
+            ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
+            image = ImageIO.read(bis);
+            bis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return image;
     }
 
     private static void generateJson() throws IOException {
@@ -68,12 +90,12 @@ public class InfoExtracter {
 
     public String getImage(String ministerName) {
         String image = null;
-        String url = KMM_CONSTANTS.GOOGLE_IMAGE_URL.replace("{QUERY_STRING_VALUE}", ministerName);
+        String url = KMM_CONSTANTS.GOOGLE_URL + ministerName;
         try {
             Document document = Jsoup.connect(url)
                     .timeout(5000)
                     .get();
-            String Image = extractAttr(document, SelectorsContants.IMAGE, "src");
+            image = extractAttr(document, SelectorsContants.IMAGE, "src");
             System.out.println(++count);
         } catch (IOException e) {
             e.printStackTrace();
@@ -131,6 +153,8 @@ public class InfoExtracter {
         if (Objects.nonNull(document)) {
             Element selectorElement = document.select(selectorConstant)
                     .first();
+            Attributes attr = selectorElement.attributes();
+            System.out.println(attr);
             if (Objects.nonNull(selectorElement)) {
                 return selectorElement.attr(attribute);
             }
